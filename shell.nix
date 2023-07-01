5cb1e8e807f1180f34a3eb66412f9fafc34f9fc8
PixelyIon <pixelyion@protonmail.com>
2023-07-01 17:15:17 +0000

    Add shell.nix for environment
    While this is a Nix-specific environment derivation, it's quite useful to have as a reference for setting up a build environment anywhere as it describes every package that's required.

---
+++ shell.nix
@@ -0,0 +1,64 @@
+{pkgs ? import <nixpkgs> {}}:
+
+let
+  llvmMingw = pkgs.stdenv.mkDerivation rec {
+    name = "llvm-mingw";
+    version = "20230517";
+
+    src = pkgs.fetchurl {
+      url = "https://github.com/mstorsjo/llvm-mingw/releases/download/${version}/llvm-mingw-${version}-ucrt-ubuntu-20.04-x86_64.tar.xz";
+      hash = "sha256-AykvzWa5/blLOQ9IWZ/y65wCTQU1c7qQiDR6nE4WHs0=";
+    };
+
+    dontStrip = true;
+    dontPatchELF = true;
+
+    nativeBuildInputs = with pkgs; [
+    ];
+
+    buildInputs = with pkgs; [
+      stdenv.cc.cc.lib
+      zlib
+    ];
+
+    installPhase = ''
+      runHook preInstall
+      mkdir -p $out
+      cp -r ./* $out
+      runHook postInstall
+    '';
+  };
+in
+  pkgs.mkShell /*.override {stdenv = pkgs.llvmPackages_latest.stdenv;}*/ {
+    NIX_LD = pkgs.lib.fileContents "${pkgs.stdenv.cc}/nix-support/dynamic-linker";
+    LLVM_MINGW_SYSROOT = llvmMingw; /* Use as -DMINGW_SYSROOT_PATH=$LLVM_MINGW_SYSROOT */
+    NIX_HARDENING_ENABLE = "0"; /* Disable hardening */
+    nativeBuildInputs = with pkgs; [
+      autoconf
+      automake
+      autogen
+      flex
+      bison
+      libtool
+      gtk-doc
+      gettext
+      pkg-config
+      gperf
+      glslang
+      libtasn1
+      python3Packages.python
+      python3Packages.mako
+      python3Packages.ply
+      libev
+      freetype
+      xorg.libX11
+      vulkan-headers
+    ];
+    depsBuildBuild = with pkgs; [
+      ninja
+      cmake
+      meson
+    ];
+  }
+
+
